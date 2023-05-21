7c10090961d5557eb9f8dbebf606b04d5d2c7125
Mikhail Bogomazov <ihackit595@gmail.com>
2023-05-21 11:42:27 +0300

    update file watching tree script

--- libs/chokidar/src/index.ts
+++ libs/chokidar/src/index.ts
@@ -1,11 +1,73 @@
 import * as chokidar from 'chokidar';
+import * as dirTree from 'directory-tree';
+
+let timeoutId: NodeJS.Timeout | null = null;
+
+type FileNode = {
+    name: string;
+    path: string;
+}
+
+type DirectoryNode = {
+    name: string;
+    path: string;
+    children: Array<FileNode | DirectoryNode>
+}
+
+export const debounce = (func: () => void, delay = 1000) => {
+    if (timeoutId) {
+        clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+        func();
+        timeoutId = null;
+    }, delay);
+}
 
 const watcher = chokidar.watch('**/*', {
     persistent: true,
-    interval: 100,
-    ignored: /(^|[\/\\])node_modules[\/\\].*/,
+    interval: 1000,
 });
 
-watcher.on('all', (event, path) => {
-    console.log(event, path);
+watcher.on('all', () => {
+    debounce(() => {
+        const rootDirNode = dirTree('.') as DirectoryNode;
+
+        console.log(JSON.stringify(sortNodes([rootDirNode])[0]));
+    }, 1000);
 });
+
+const sortNodes = (rootNode: Array<FileNode | DirectoryNode>): Array<FileNode | DirectoryNode> => {
+    rootNode.sort((a, b) => {
+        if (isDirectory(a) && isDirectory(b)) {
+            return a.name.localeCompare(b.name);
+        }
+
+        if (isDirectory(a)) {
+            return -1;
+        }
+
+        if (isDirectory(b)) {
+            return 1;
+        }
+
+        if (a.name.length !== b.name.length) {
+            return a.name.length - b.name.length;
+        }
+
+        return a.name.localeCompare(b.name);
+    })
+
+    for (const node of rootNode) {
+        if (isDirectory(node)) {
+            node.children = sortNodes(node.children);
+        }
+    }
+
+    return rootNode;
+}
+
+export const isDirectory = (node: FileNode | DirectoryNode): node is DirectoryNode => {
+    return 'children' in node;
+}
